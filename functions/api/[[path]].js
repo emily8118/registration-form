21954ac58a98f43916094e52ff0d38ae01557d15
emily8118 <emily.fw8118@gmail.com>
2026-02-11 20:48:02 -0500

    re-add version dropdown

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -54,7 +54,7 @@ export async function onRequestPost(context) {
         try {
             // Parse the incoming JSON data
             const data = await request.json();
-            const { name, email, phone} = data;
+            const { name, email, phone,  version} = data;
 
             // Validate input
             if (!name || !email) {
@@ -121,6 +121,7 @@ export async function onRequestPost(context) {
                 name,
                 email,
 				phone: phone || '',
+				version, 
                 registeredAt: new Date().toISOString(),
                 id: crypto.randomUUID()
             };
